cae542fef4af7fb9858e18b3e92411403b51b1a2
Ulises Santana <ulisesantana@gmail.com>
2019-05-04 23:19:53 +0100

    basic MVP - pending data

---
+++ src/components/App/App.tsx
@@ -0,0 +1,67 @@
+import React, {ChangeEvent, useState} from 'react';
+import {Grid, withStyles} from "@material-ui/core";
+import {Nav} from "../Nav"
+import {ingredients, categories} from "./data";
+import {IngredientsList} from "../IngredientsList";
+import {CategoryID, Frequency} from "../../types";
+import {isMobile} from "../../utils";
+
+const style = {
+  root: {
+    padding: "8px 16px 30px 16px"
+  }
+};
+
+const getIngredientsByCategoryAndFrequency = (id: CategoryID, freq: Frequency) =>
+  Object.values(ingredients)
+    .filter(({category, frequency}) =>
+      category === id && frequency === freq
+    );
+
+
+export const App = withStyles(style)(({classes}: {classes: Record<"root", string>}) => {
+  const [section, setSection] = useState(0);
+  const onChangeSection = (e: ChangeEvent<{}>, index: number) => {
+    setSection(index);
+  };
+
+  return (
+    <Grid>
+      <Nav value={section} onChange={onChangeSection}/>
+      <Grid
+        container
+        className={`${isMobile() ? classes.root : ''}`}
+        direction={isMobile() ? 'column' : 'row'}
+        justify={'space-between'}
+        alignItems="center"
+        spacing={8}>
+        {section === 0 && categories.map(({id, label}) =>
+          <IngredientsList
+            key={id}
+            category={label}
+            img={'http://placekitten.com/300/100'}
+            ingredients={getIngredientsByCategoryAndFrequency(id, Frequency.daily)}
+          />
+        )}
+        {section === 1 && categories.map(({id, label}) =>
+          <IngredientsList
+            key={id}
+            category={label}
+            img={'http://placekitten.com/300/100'}
+            ingredients={getIngredientsByCategoryAndFrequency(id, Frequency.weekly)}
+          />
+        )}
+        {section === 2 && categories.map(({id, label}) =>
+          <IngredientsList
+            key={id}
+            category={label}
+            img={'http://placekitten.com/300/100'}
+            ingredients={getIngredientsByCategoryAndFrequency(id, Frequency.never)}
+          />
+        )}
+      </Grid>
+    </Grid>
+  );
+});
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import * as serviceWorker from './serviceWorker';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#524aac',
+      main: '#19227c',
+      dark: '#00004f',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff6434',
+      main: '#dd2c00',
+      dark: '#a30000',
+      contrastText: '#fff',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  }
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+  <App />
+</MuiThemeProvider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
