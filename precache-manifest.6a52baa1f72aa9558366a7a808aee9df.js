ce9f49e0a8e57e6329d76f57ab5d5c4100ccc84f
Ulises Santana <ulisesantana@gmail.com>
2019-05-04 23:09:49 +0100

    Updates

---
+++ precache-manifest.6a52baa1f72aa9558366a7a808aee9df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b4fc92ca39a712dc1d93ac018a58aa",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "de075e74d3076fa7162e",
+    "url": "/repository-name/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "7bde62efe9da6e32674c",
+    "url": "/repository-name/static/js/2.8325ac22.chunk.js"
+  },
+  {
+    "revision": "de075e74d3076fa7162e",
+    "url": "/repository-name/static/js/main.b362298f.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  }
+]);
